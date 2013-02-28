00938ecdca363a7b04ed985fb0a0933e3818581c
avoid3d <avoid3d@gmail.com>
2013-02-28 16:22:23 +0200

    added pontus

---
+++ pontus/pontus.py
@@ -0,0 +1,25 @@
+import pymongo
+from gridfs import GridFS
+from bson import ObjectId
+import acoustid
+from pprint import pprint
+
+db = pymongo.Connection('localhost', 27017)['test']
+
+while(True):
+    queue_entry = db.queue.find_and_modify(
+        query={"stage": "metadata"},
+        sort={"priority": -1},
+        update={"$set": {"stage": "metadata_inprogress"}},
+        new=True
+    )
+    if (queue_entry):
+        audio_file = db.fs.files.find_one({'_id': ObjectId(queue_entry['file_id'])})
+        fingerprint = audio_file['fingerprint']
+        duration = audio_file['duration']
+
+        results = acoustid.lookup('cSpUJKpD', fingerprint, duration)['results']
+        music_brainz_id = results[0]['recordings'][0]['id']
+        title = results[0]['recordings'][0]['title']
+
+        db.songs.insert({'music_brainz_id': music_brainz_id, 'music_brainz_title': title})
