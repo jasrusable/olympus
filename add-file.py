ce5a0954b85f8b3855ac059abb9312b153d7b113
Pierre Hugo <avoid3d@gmail.com>
2013-03-07 16:41:01 +0200

    fixed broken add-file.py script

--- add-file.py
+++ add-file.py
@@ -15,10 +15,10 @@ new_file = fs.new_file(filename=filename)
 # myFile.file.read reads from that.
 size = 0
 while True:
-    data = audio_file.read(8192)
-    if not data:
-        break
-    new_file.write(data)
+    data = audio_file.read(8192)
+    if not data:
+        break
+    new_file.write(data)
 
 new_file.close()
 db.queue.insert({'stage': 'fingerprint', 'priority': 4, 'file_id': new_file._id})
